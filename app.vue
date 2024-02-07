0c3051f928a2cade8711d7fb84e9aafc1795593b
petedavisdev <pete.davis@gospace.com>
2024-02-07 10:38:23 +0000

    nuxt veevalidate zod example

---
+++ app.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { useForm } from 'vee-validate';
+import { toTypedSchema } from '@vee-validate/zod';
+import { z } from 'zod';
+
+const { errors, defineField } = useForm({
+	validationSchema: toTypedSchema(
+		z.object({
+			email: z.string().min(1).email(),
+		})
+	),
+});
+
+const [email, emailAttrs] = defineField('email');
+</script>
+
+<template>
+	<AppField name="email" label="Email">
+		<input v-model="email" v-bind="emailAttrs" />
+	</AppField>
+</template>
---
+++ components/AppField.vue
@@ -0,0 +1,24 @@
+<template>
+	<div>
+		<label>
+			{{ props.label }}
+		</label>
+
+		<slot />
+
+		<p>
+			{{ errorMessage }}
+		</p>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { useField } from 'vee-validate';
+
+const props = defineProps<{
+	name: string;
+	label: string;
+}>();
+
+const { errorMessage } = useField(() => props.name);
+</script>
